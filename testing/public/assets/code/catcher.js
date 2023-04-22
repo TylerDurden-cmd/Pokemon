a4bdda96720f6a4ac797242df60ca3490ede0750
TylerDurden-cmd <125083855+TylerDurden-cmd@users.noreply.github.com>
2023-04-22 22:13:24 +0200

    Nog paar Modifiicaties aan PokemonCatcher

--- testing/public/assets/code/catcher.js
+++ testing/public/assets/code/catcher.js
@@ -1,7 +1,8 @@
 let btn = document.querySelector(".buttoncatch")
 let pokebal = document.querySelector(".pokebal")
 let Pokemon = document.querySelector(".RandomPokemon")
-
+let counter = document.querySelector(".countcontainer")
+let PokemonCaught = document.querySelector(".PokemonCaughtContainer")
 
 const ColorGreen = () =>{
     pokebal.style.backgroundColor = "Green"
@@ -12,7 +13,12 @@ const ColorRed = () =>{
     pokebal.style.backgroundColor = "Red"
 }
 
+
+counter.textContent = 3;
+
 btn.addEventListener('click',async(e)=>{
+    counter.textContent--;
+
     try{
   const take = await(await fetch(`https://pokeapi.co/api/v2/pokemon/${Pokemon.textContent}`)).json()
   let EigenPokemnoAttack = 48;
@@ -20,7 +26,9 @@ btn.addEventListener('click',async(e)=>{
   const PercentageVoorPokemonCatcher = 100 - EigenPokemnoAttack + PokemonDefenceStats;
   const PercentageVoorPokemonCatcherBerekend = Math.floor(Math.random() * PercentageVoorPokemonCatcher)
   if(EigenPokemnoAttack > PercentageVoorPokemonCatcherBerekend){
+
     ColorGreen();
+    PokemonCaught.innerHTML = `Congrats You Caught ${Pokemon.textContent}!!`
   }else{
     ColorRed();
   }}catch(error){
