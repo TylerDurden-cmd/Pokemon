0de0ae415563120ebb10a3e99ba09610e5de4c10
Christiaan Uytterhoeven <christiaan.uytterhoeven@student.ap.be>
2023-04-24 21:09:57 +0200

    verder gewerkt aan de comparer

--- testing/public/assets/code/compare.js
+++ testing/public/assets/code/compare.js
@@ -1,11 +1,9 @@
-import fetch from 'node-fetch';
-
 //adhv de index van de pokemon haalt het de stats (getallen) op
 const PokemonStatsNumbers = async (currentPokemon) =>{
     let PokemonStatsNum = [];
     let pokemonUrl = (await fetch(`https://pokeapi.co/api/v2/pokemon/${currentPokemon}`)).json();
 
-    for (let index = 0; index < pokemonUrl.stats.length; index++) {   
+    for (let index = 0; index < pokemonUrl.stats; index++) {   
         PokemonStatsNum[index] = pokemonUrl.stats[index].base_stat;
     }
 
@@ -16,7 +14,7 @@ const PokemonStatsNames = async () =>{
     let PokemonStatsNames = [];
     let pokemonUrl = (await fetch(`https://pokeapi.co/api/v2/pokemon/1`)).json();
 
-    for (let index = 0; index < pokemonUrl.stats.length; index++) {   
+    for (let index = 0; index < pokemonUrl.stats; index++) {   
         PokemonStatsNames[index] = pokemonUrl.stats[index].stat.name;       
     }
 
@@ -26,7 +24,7 @@ const PokemonStatsNames = async () =>{
 //functie wordt uitgevoerd elke keer als de value van een dropdown veranderd bij de comparer
 const OnChange = async () => {
     //geeft de op dit moment geselecteerde pokemon in de dropdowns
-    let currentPokemonLeft = document.getElementById("pokeLeft").value;    
+    let currentPokemonLeft = document.getElementById("pokeleft").value;    
     //let currentPokemonRight = document.getElementById("pokeRight").value;
     //arrays van de stats van de huidige pokemon en de namen van de stats
     let leftStatsNum = await PokemonStatsNumbers(currentPokemonLeft);
@@ -36,7 +34,7 @@ const OnChange = async () => {
     //linkse pokemon stats inlezen in section
     for (let index = 0; index < statNames.length; index++) {
         let stat = `$<span>${statNames[index]} ${leftStatsNum[i]}</span><br>`;
-        document.getElementById("left").insertAdjacentHTML("afterbegin", stat)        
+        document.getElementById("left").innerHTML += `$<span>${statNames[index]} ${leftStatsNum[i]}</span><br>`;        
     }
 
     //rechtse pokemon stats inlezen in section
